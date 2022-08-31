96ee3f91c292c6750553f567e0d663d6e8864f1f
juliaspepe <devjuliapepe@gmail.com>
2022-08-31 03:28:02 -0300

    criação do quizz - info básicas (início)

--- data/scripts/main.js
+++ data/scripts/main.js
@@ -12,4 +12,20 @@ promise.then(
 )
 function iniciarCriarQuizz(){
     document.querySelector(".tela1").classList.add("disable");
+    primeiraParteCriacaoQuizz();
+}
+
+function primeiraParteCriacaoQuizz() {
+    document.querySelector(".passoUmCriacaoQuizz").innerHTML = 
+
+    `<h1>Comece pelo começo</h1>
+
+    <div class="inputsPassoUm">
+    <input type="text" placeholder="Título do seu quizz" class="titulo" />
+    <input type="text" placeholder="URL da imagem do seu quizz" class="URL" />
+    <input type="text" placeholder="Quantidade de perguntas do quizz" class="qdPerguntas" />
+    <input type="text" placeholder="Quantidade de níveis do quizz" class="niveis" />
+    </div>
+    
+    <button onclick="criarPerguntas()">Prosseguir pra criar perguntas</button>`
 }
