9ce698053d17781292c52b5ce9720b3726d98489
Wesley Dias <wesley.dias.dev@gmail.com>
2022-09-03 20:05:10 -0300

    Adicionado a interação de tela quando o usuário clica numa resposta, depois de 2s vai automaticamente pra próxima!

---
+++ data/scripts/scrollQuizz.js
@@ -0,0 +1,11 @@
+function scrollarProxima(li) {
+  const proximoElemento = Number(li.className.replace('respostaScroll','')) + 1;
+  console.log(proximoElemento)
+  const elementoScroll = document.querySelector(`.respostaScroll${proximoElemento}`)
+  if(elementoScroll){
+    setTimeout(() => {
+      elementoScroll.scrollIntoView({behavior: 'smooth'})
+    }, 2000)
+  }
+  
+}
